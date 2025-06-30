0d9e69100cbf4607cf7fe8764fe773ccb04acfcd
Mohammad Mushfiq Us Saleheen <aurpon80@gmail.com>
2025-06-30 18:37:00 +0600

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -204,7 +204,7 @@ export default function AboutPage() {
             <Card className="overflow-hidden">
               <CardContent className="p-0">
                 <Image
-                  src="/placeholder.jpg"
+                  src="https://images.unsplash.com/photo-1624555130581-1d9cca783bc0?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="Our organic farm partners"
                   width={600}
                   height={400}
